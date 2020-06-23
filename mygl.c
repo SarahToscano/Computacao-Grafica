0e97826ead9a6402d51fbe8bb5291e96448c229c
Sarah de Carvalho <33987228+SAndradeTC@users.noreply.github.com>
2020-06-23 09:29:21 -0300

    Add files via upload

---
+++ mygl.c
@@ -0,0 +1,14 @@
+#include "mygl.h"
+
+//
+// >>> Defina aqui as funções que você implementar <<< 
+//
+
+// Definição da função que chamará as funções implementadas pelo aluno
+void MyGlDraw(void) {
+
+    //
+    // >>> Chame aqui as funções que você implementou <<<
+    //
+
+}
---
+++ mygl.h
@@ -0,0 +1,14 @@
+#ifndef MYGL_H
+#define MYGL_H
+
+#include "core.h"
+#include "frame_buffer.h"
+
+// Declaração da função que chamará as funções implementadas pelo aluno
+void MyGlDraw(void);
+
+//
+// >>> Declare aqui as funções que você implementar <<<
+//
+
+#endif  // MYGL_H
