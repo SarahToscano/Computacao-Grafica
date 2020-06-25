04d7a8283fbb15d12f182b6fe3e619f9ada6d2af
Sarah de Carvalho <33987228+SAndradeTC@users.noreply.github.com>
2020-06-25 03:32:58 -0300

    Update mygl.h

--- mygl.h
+++ mygl.h
@@ -4,11 +4,37 @@
 #include "core.h"
 #include "frame_buffer.h"
 
-// Declaração da função que chamará as funções implementadas pelo aluno
+typedef struct {
+    unsigned int r;
+    unsigned int g;
+    unsigned int b;
+    unsigned int a;
+
+}Color;
+
+typedef struct {
+    int x1;
+    int x2;
+    int x3;
+    int y1;
+    int y2;
+    int y3;
+
+}Coordinates;
+
 void MyGlDraw(void);
+
+void PutPixel(int x1, int y1, Color *reference);
+void DrawLine (int x1, int y1, int x2, int y2, Color *A, Color *B, Color *Color_reference);
+
+float Distance(int x1, int y1, int x2, int y2);
+void ColorGradient(int x1, int y1, int x2, int y2, Color *A, Color *B, Color *C);
+
+
 
 //
 // >>> Declare aqui as funções que você implementar <<<
 //
 
 #endif  // MYGL_H
+
