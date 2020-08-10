b22b1dd5ef8ed1be6439bde4a5947f1e8197c53f
Sarah de Carvalho <satc1624@gmail.com>
2020-08-10 11:24:39 -0300

    atividade4

---
+++ Atividade_4/04_shading/vertex_shader.glsl
@@ -0,0 +1,37 @@
+#version 330 core
+
+layout (location = 0) in vec3 obj_spc_vertex_pos;
+layout (location = 1) in vec3 obj_spc_N;
+layout (location = 2) in vec3 k_d;
+
+uniform mat4 model_mat;
+uniform mat4 view_mat;
+uniform mat4 proj_mat;
+
+out vec3 I; 
+
+void main() {
+    vec3 cam_pos = vec3(0.0f, 0.0f, 1.5f);
+
+    vec3 I_a = vec3(0.2f, 0.2f, 0.2f);
+    vec3 k_a = vec3(0.0f, 0.0f, 1.0f);	
+    vec3 k_s = vec3(1.0f, 1.0f, 1.0f);	
+    vec3 I_p_pos = vec3(-2.0f, 2.0f, 1.5f);
+    vec3 I_p = vec3(0.8f, 0.8f, 0.8f);
+    float n = 64;
+
+    vec3 L = normalize (I_p_pos-(model_mat*vec4(obj_spc_vertex_pos, 1.0)).xyz);
+    vec3 N = normalize (mat3(transpose(inverse(model_mat)))*obj_spc_N);
+
+    float cos_theta = dot(L,N);
+
+    vec3 R = - reflect (L , N ) ;
+    vec3 V = normalize (cam_pos - (model_mat*vec4(obj_spc_vertex_pos, 1.0)).xyz);
+    
+    float cos_alfa= dot(R,V);
+    
+    I = I_a*k_a + I_p*(k_d*cos_theta + k_s*(pow(cos_alfa,n)));
+
+
+    gl_Position = proj_mat * view_mat * model_mat * vec4(obj_spc_vertex_pos, 1.0);
+}
